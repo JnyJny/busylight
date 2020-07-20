aef026840df3300ead5b965d7195ecc642e58b6d
JnyJny <erik.oshaughnessy@gmail.com>
2020-07-19 20:27:50 -0500

    Refined API, still needs error handling and proper return values

--- busylight/api/__init__.py
+++ busylight/api/__init__.py
@@ -3,100 +3,122 @@
 
 import asyncio
 
-from fastapi import FastAPI, BackgroundTasks
+from fastapi import FastAPI, Path
 
 from ..effects import rainbow, throbber, flash_lights_impressively
 from ..lights.manager import LightManager
 
-app = FastAPI()
-
-manager = LightManager()
-
-manager.light_off()
+api = FastAPI()
 
 
-@app.get("/supported")
+@api.on_event("startup")
+async def startup():
+    api.manager = LightManager()
+    api.manager.light_off()
+
+
+@api.on_event("shutdown")
+async def shutdown():
+    api.manager.light_off()
+
+
+@api.get("/supported")
 async def Supported_Lights():
+    """List of supported lights (may not be currently connected).
     """
-    """
-    return {"supported": manager.supported}
+    return {"supported": api.manager.supported}
 
 
-@app.get("/available")
+@api.get("/available")
 async def Available_Lights():
+    """List of lights currently connected. 
     """
-    """
-    return manager.available
+    return api.manager.available
 
 
-@app.get("/helpers")
+@api.get("/helpers")
 async def Helper_Threads():
+    """Debug: lists of helper threads and the light they are associated with.
+    """
+
+    return [light.name for light in api.manager.managed if light.helper_thread]
+
+
+@api.get("/light/{light_id}/on")
+async def Turn_On_Light(light_id: int = Path(..., title="Light identifier", ge=1)):
     """
     """
-    helpers = {}
-    for key, value in manager.helpers.items():
-        helpers.setdefault(key, value.is_alive())
-
-    return helpers
+    api.manager.light_on(light_id)
 
 
-@app.get("/light/{light_id}/on")
-async def Turn_On_Light(light_id: int):
+@api.get("/light/{light_id}/on/{color}")
+async def Turn_On_Light_With_Color(
+    light_id: int = Path(..., title="Light identifier", ge=1),
+    color: str = Path(..., title="Color specifier string"),
+):
+    """Turn on a specific light with the given color. 
+    
+    The color can be a color nam or a hexadecimal
+    string. Hexadecimal strings can be prefixed with # or 0x
+    and can be 3 or 6 digits long. 
+
+    Example color usage that all result in light zero being
+    activated with the color "red":
+
+    /light/0/on/red 
+    /light/0/on/0xff0000
+    /light/0/on/#ff0000
+    /light/0/on/#f00
+
+    :param light_id: integer in the range [0..NLIGHTS-1]
+    :param color: string
     """
-    """
-    manager.light_on(light_id)
+    api.manager.light_on(light_id, color)
 
 
-@app.get("/light/{light_id}/on/{color}")
-async def Turn_On_Light_With_Color(light_id: int, color: str = "green"):
-    """
-    """
-    manager.light_on(light_id, color)
-
-
-@app.get("/lights/on")
+@api.get("/lights/on")
 async def Turn_On_Lights():
+    """Turn on all lights with the default color, green.
     """
-    """
-    manager.light_on(-1)
+    api.manager.light_on(-1)
 
 
-@app.get("/lights/on/{color}")
+@api.get("/lights/on/{color}")
 async def Turn_On_Lights_With_Color(color: str = "green") -> dict:
     """
     """
-    manager.light_on(-1, color)
+    api.manager.light_on(-1, color)
 
 
-@app.get("/light/{light_id}/off")
+@api.get("/light/{light_id}/off")
 async def Turn_Off_Light(light_id: int) -> dict:
     """
     """
-    manager.light_off(light_id)
+    api.manager.light_off(light_id)
 
 
-@app.get("/lights/off")
+@api.get("/lights/off")
 async def Turn_Off_Lights() -> dict:
     """
     """
-    manager.light_off(-1)
+    api.manager.light_off(-1)
 
 
-@app.get("/light/{light_id}/blink")
+@api.get("/light/{light_id}/blink")
 async def Blink_Light(light_id: int) -> dict:
     """
     """
-    manager.light_blink(light_id)
+    api.manager.light_blink(light_id)
 
 
-@app.get("/light/{light_id}/blink/{color}")
+@api.get("/light/{light_id}/blink/{color}")
 async def Blink_Light_With_Color(light_id: int, color: str = "red") -> dict:
     """
     """
-    manager.blink_light(light_id, color)
+    api.manager.light_blink(light_id, color)
 
 
-@app.get("/light/{light_id}/blink/{color}/{speed}")
+@api.get("/light/{light_id}/blink/{color}/{speed}")
 async def Blink_Light_With_Color_and_Speed(
     light_id: int, color: str = "red", speed: int = 1
 ) -> dict:
@@ -105,29 +127,36 @@ async def Blink_Light_With_Color_and_Speed(
     manaber.light_blink(light_id, color, speed)
 
 
-@app.get("/lights/blink")
+@api.get("/lights/blink")
 async def Blink_Lights() -> dict:
     """
     """
-    manager.light_blink(-1)
+    api.manager.light_blink(-1)
 
 
-@app.get("/lights/blink/{color}")
+@api.get("/lights/blink/{color}")
 async def Blink_Lights_With_Color(color: str = "red", speed: int = 1) -> dict:
     """
     """
-    manager.light_blink(-1, color)
+    api.manager.light_blink(-1, color)
 
 
-@app.get("/lights/blink/{color}/{speed}")
+@api.get("/lights/blink/{color}/{speed}")
 async def Blink_Lights_With_Color_and_Speed(color: str = "red", speed: int = 1) -> dict:
     """
     """
-    manager.light_blink(-1, color, speed)
+    api.manager.light_blink(-1, color, speed)
 
 
-@app.get("/light/{light_id}/rainbow")
+@api.get("/light/{light_id}/rainbow")
 async def Rainbow_Light(light_id: int) -> dict:
     """
     """
-    manager.apply_effect_to_light(light_id, rainbow)
+    api.manager.apply_effect_to_light(light_id, rainbow)
+
+
+@api.get("/lights/rainbow")
+async def Rainbow_Lights():
+    """
+    """
+    api.manager.apply_effect_to_light(-1, rainbow)
