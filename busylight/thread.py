356f2e1f4937052f0879e99fc10653b7db8d7375
JnyJny <erik.oshaughnessy@gmail.com>
2020-07-21 23:29:33 -0500

    Further refinements to the Cancellable thread class

--- busylight/thread.py
+++ busylight/thread.py
@@ -1,25 +1,29 @@
-"""A thread for controlling USBLight effects.
+"""A threading.Thread subclass that can be cancelled.
+
 """
 
 from threading import Thread
 from typing import Generator
 
 
-class EffectThread(Thread):
+class CancellableThread(Thread):
     def __init__(self, target: Generator, name: str = None):
         """
+        :param target: generator
+        :param name: str
         """
+        if not isinstance(target, Generator):
+            raise ValueError("Target must be a generator.")
+
         super().__init__(target=target, name=name, daemon=True)
         self._is_cancelled = False
 
     def run(self) -> None:
         """Executes the `target` generator until the thread is cancelled.
 
-        The `target` is expected to perform a long running operation
-        that periodically calls yield to allow the EffectThread to
-        check if it's been cancelled. 
-
-        :param target: Generator that yields
+        The `target` generator is expected to perform a long running operation
+        that periodically calls "yield" to allow the thread to check if
+        it has been cancelled.
 
         ```python
         def long_running(interval:int = 1) -> None:
@@ -31,15 +35,17 @@ class EffectThread(Thread):
         
         """
         while True:
-            for _ in self._target():
+            for _ in self._target:
                 if self._is_cancelled:
                     return
 
-    def cancel(self) -> None:
-        """Stops the thread provided the `target` callable is yielding
-        control to the `EffectThread.run` method at a reasonable rate.
-        The sooner the target yields, the quicker the thread will
-        cancel. Once a thread is cancelled, the caller will need to
-        call `join` to clean up the thread. 
+    def cancel(self, join: bool = True, timeout: float = 0.05) -> None:
+        """Signals to the `run` method that the thread should terminate.
+
+        :param join: bool
+        :param timeout: float
         """
         self._is_cancelled = True
+        if join:
+            while self.is_alive():
+                self.join(timeout)
