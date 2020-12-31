d99849442f7243b577b0c48f6ad3883bc65d78b6
JnyJny <erik.oshaughnessy@gmail.com>
2020-12-30 22:22:28 -0600

    ABC USBLight updates
    Re-implemented KNOWN_VENDOR_IDS and SUPPORTED_LIGHTS as classmethods
    on the USBLight class and updated the module to use the new methods.

--- busylight/lights/__init__.py
+++ busylight/lights/__init__.py
@@ -11,13 +11,10 @@ import hid
 ## 0. Create a new light class subclassed from busylight.light.USBLIght
 ##    - implement 'on', 'off', 'blink' methods at a minimum
 ##    - define class attribute list VENDOR_IDS
-##    - define class asttribute string __vendor__
-##
-##    Use the existing light subclasses as inspiration.
+##    - define class attribute list PRODUCT_IDS
+##    - define class asttribute string vendor
 ##
 ## 1. Import the module for the new light here in this file.
-## 2. Add the new class to the SUPPORTED_LIGHTS list
-## 3. Add the name of the new class to the __all__ list
 
 
 from .usblight import USBLight
@@ -27,19 +24,15 @@ from .exceptions import USBLightUnknownProduct
 from .exceptions import USBLightInUse
 from .exceptions import USBLightIOError
 
+# EJO some kind of cool dynamic import would be sweet right here
+
 from .agile_innovations import BlinkStick
 from .embrava import Blynclight
 from .kuando import BusyLight
 from .luxafor import Flag
 from .thingm import Blink1
 
-SUPPORTED_LIGHTS = USBLight.supported_lights()
-
-KNOWN_VENDOR_IDS = sum([l.VENDOR_IDS for l in SUPPORTED_LIGHTS], [])
-
 __all__ = [
-    "KNOWN_VENDOR_IDS",
-    "SUPPORTED_LIGHTS",
     "USBLight",
     "USBLightNotFound",
     "USBLightUnknownVendor",
