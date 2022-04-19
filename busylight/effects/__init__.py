0f41039e7bfa3135a2a64142efd096ed318045dd
JnyJny <erik.oshaughnessy@gmail.com>
2022-04-18 20:56:19 -0500

    Removed Throb effect, Gradient has it covered

--- busylight/effects/__init__.py
+++ busylight/effects/__init__.py
@@ -1,14 +1,12 @@
 """Color Generators
 """
 
-
 from .effect import BaseEffect as Effects
 
 from .blink import Blink
 from .gradient import Gradient
 from .spectrum import Spectrum
 from .steady import Steady
-from .throb import Throb
 
 __all__ = [
     "Effects",
@@ -16,5 +14,4 @@ __all__ = [
     "Gradient",
     "Spectrum",
     "Steady",
-    "Throb",
 ]
