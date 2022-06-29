bd0d03f31274eedff6e3e9592c4f9c249bcd61dc
JnyJny <erik.oshaughnessy@gmail.com>
2022-06-29 14:39:16 -0500

    YAGNI: Removed steady effect

--- busylight/effects/__init__.py
+++ busylight/effects/__init__.py
@@ -6,12 +6,10 @@ from .effect import BaseEffect as Effects
 from .blink import Blink
 from .gradient import Gradient
 from .spectrum import Spectrum
-from .steady import Steady
 
 __all__ = [
     "Effects",
     "Blink",
     "Gradient",
     "Spectrum",
-    "Steady",
 ]
