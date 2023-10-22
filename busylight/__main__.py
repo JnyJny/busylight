8b5ba18935fc62b4ddd7dc3e1880a9be72565f24
JnyJny <erik.oshaughnessy@gmail.com>
2023-10-22 12:01:04 -0500

    Removed busylight.api and references

--- busylight/__main__.py
+++ busylight/__main__.py
@@ -21,8 +21,6 @@ from .lights import Light
 
 cli = typer.Typer()
 
-webcli = typer.Typer()
-
 
 @dataclass
 class GlobalOptions:
@@ -361,50 +359,6 @@ def generate_udev_rules(
     ]
 
     print("\n".join(about + rules), file=output)
-
-
-@webcli.command()
-def serve_http_api(
-    debug: bool = typer.Option(False, "--debug", "-D", is_flag=True),
-    host: str = typer.Option(
-        "0.0.0.0",
-        "--host",
-        "-h",
-        help="Host name to bind the server to.",
-    ),
-    port: int = typer.Option(
-        8000,
-        "--port",
-        "-p",
-        help="Network port number to listen on.",
-    ),
-) -> None:
-    """Serve a HTTP API to access available lights."""
-
-    environ["BUSYLIGHT_DEBUG"] = str(debug)
-
-    (logger.enable if debug else logger.disable)("busylight")
-    logger.info("serving http api")
-
-    try:
-        import uvicorn
-    except ImportError as error:
-        logger.error(f"import uvicorn failed: {error}")
-        typer.secho(
-            "The package `uvicorn` is missing, unable to serve the busylight API.",
-            fg="red",
-        )
-        raise typer.Exit(code=1) from None
-
-    try:
-        uvicorn.run("busylight.api:busylightapi", host=host, port=port, reload=debug)
-    except ModuleNotFoundError as error:
-        logger.error(f"Failed to start webapi: {error}")
-        typer.secho(
-            "Failed to start the webapi.",
-            fg="red",
-        )
-        raise typer.Exit(code=1) from None
 
 
 if __name__ == "__main__":
