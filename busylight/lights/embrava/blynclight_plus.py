000601764b1734fdfd329a850ea5e48de78dcb1f
JnyJny <erik.oshaughnessy@gmail.com>
2023-12-05 14:06:58 -0600

    Addresses issue #306 - added new Blynclight Plus product id 02

--- busylight/lights/embrava/blynclight_plus.py
+++ busylight/lights/embrava/blynclight_plus.py
@@ -11,5 +11,6 @@ class Blynclight_Plus(Blynclight):
     @staticmethod
     def supported_device_ids() -> Dict[Tuple[int, int], str]:
         return {
+            (0x2C0D, 0x0002): "Blynclight Plus",
             (0x2C0D, 0x0010): "Blynclight Plus",
         }
