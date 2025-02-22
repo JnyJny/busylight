2d275bf460cdd6bdaf7306e71e346cacb22d62da
Tobias Gall <tga@hylastix.com>
2025-02-22 21:41:21 +0100

    Add Support for SIA Busy Tag
    This PR adds basic LED support for SIA Busy Tag.
    
    Basic as in:
    
    * addressing all LEDs only (7 LEDs are present)
    * no support for the integrated LCD Display
    
    https://www.busy-tag.com/
    
    LED can be controlled using serial connection and AT commands as
    descibed in this guide: https://luxafor.helpscoutdocs.com/article/47-busy-tag-usb-cdc-command-reference-guide

---
+++ busylight/lights/siabusytag/__init__.py
@@ -0,0 +1,9 @@
+"""SIA Busy Tag Support
+
+https://www.busy-tag.com/
+
+"""
+
+from .sia_busy_tag import SiaBusyTag
+
+__all__ = ["SiaBusyTag"]
---
+++ busylight/lights/siabusytag/sia_busy_tag.py
@@ -0,0 +1,30 @@
+"""
+"""
+
+from typing import Dict, Tuple
+
+from loguru import logger
+
+from ..seriallight import SerialLight
+
+
+class SiaBusyTag(SerialLight):
+    @staticmethod
+    def supported_device_ids() -> Dict[Tuple[int, int], str]:
+        return {
+            (0x303a, 0x81df): "GREYNUT LTD BUSY TAG",
+        }
+
+    @staticmethod
+    def vendor() -> str:
+        return "SIA Busy Tag"
+
+    def __bytes__(self) -> bytes:
+
+        # Change Color using AT Serial Commands 
+        # See for reference:
+        # https://luxafor.helpscoutdocs.com/article/47-busy-tag-usb-cdc-command-reference-guide
+        # AT+SC=127,<hex color>
+        buf = f"AT+SC=127,{self.red:02x}{self.green:02x}{self.blue:02x}"
+
+        return buf.encode()
